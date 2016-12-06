e476afe26a55b8cc0506fac3881f9d9176786e58
Erik Clineschmidt <eclineschmidt@gmail.com>
2016-12-06 13:56:22 -0800

    fini

--- static/js/db.js
+++ static/js/db.js
@@ -11,8 +11,4 @@ var ItemSchema = new mongoose.Schema({
  age: {type: Number}
 });
 
-// mongoose.model('User', UserSchema); // We are setting this Schema in our Models as 'User'
-// var User = mongoose.model('User') // We are retrieving this Schema from our Models, named 'User'
-// module.exports = User;
-// Instead, do this...
 module.exports = mongoose.model('item',ItemSchema);
