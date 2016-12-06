631f69ede3a057f5483af1f5a60d89ffbc9cae44
Erik Clineschmidt <eclineschmidt@gmail.com>
2016-12-05 16:53:38 -0800

    wrks

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
 var port = 8000;
 
 // for parsing the POST body
@@ -9,43 +10,24 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(__dirname + '/static'));
 // set the views directory
 app.set('views', __dirname + '/views');
-// use the EJS JS templating engine
+// set EJS as the templating engine
 app.set('view engine','ejs');
+// connect to MongoDB
+mongoose.connect('mongodb://localhost/basic_mongoose',function(){
+   console.log('mongoose connected');
+});
+
 
 // ROUTES --------------------------------------
 app.get('/', function (req, res){
-  res.render('index');
+   data = [{
+      name: "erik",
+      age: "10"
+   }];
+  res.render('index',{users: data});
 });
 
 // BEGIN listening for requests -----------------
 var server = app.listen(port,function(){
    console.log('Listening on port %d',port);
 });
-
-var count = 0;
-
-// SOCKET IO CONNECTIONS -------------------------
-var io = require('socket.io').listen(server);
-io.sockets.on('connection', function (socket) {
-   // pass the current count when the user connects
-   data = { count: count };
-   socket.emit('server_update', data);
-
-  socket.on("button_clicked", function (data){
-      console.log(socket.id);
-      // pass thru the data sent by the form
-      count += 1;
-      var data = {
-         count: count
-      };
-      io.emit('server_update', data);
-  })
-  socket.on("reset_count", function (data){
-   console.log(socket.id);
-      count = 0;
-      var data = {
-         count: count
-      };
-      io.emit('server_update', data);
-  })
- })
--- static/js/main.js
+++ static/js/main.js
@@ -1,23 +1,9 @@
 $(document).ready(function (){
-   // this triggers the connection event in our server!
-   var socket = io.connect();
-   var results = $('#results');
 
-   results.html('')
+   // var results = $('#results');
+   //
+   // results.html('')
 
-   // send
-   $('#add').submit(function(e){
-      e.preventDefault();
-      socket.emit('button_clicked');
-   });
-   $('#reset_count').submit(function(e){
-      e.preventDefault();
-      socket.emit('reset_count');
-   });
-   // receive
-   socket.on('server_update',function(data){
-      console.log('Server says: ' + data.count);
-      results.html(JSON.stringify(data.count));
-   })
 
-})
+
+});
